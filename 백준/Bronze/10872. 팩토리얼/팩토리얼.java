0092461a0d66aff68352ab524d7dd83c8f6dbdd9
Seol A <dlcodus1201@naver.com>
2025-03-06 23:15:41 +0900

    [Bronze III] Title: 팩토리얼, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 0~12
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         int result = 1;
 
         for (int i = 1; i <= N; i++) {
