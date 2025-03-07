b5cdfa9d9816f5b13070e7603056c58a335b7710
Seol A <dlcodus1201@naver.com>
2025-03-07 19:51:28 +0900

    [Bronze V] Title: A+B - 8, Time: 148 ms, Memory: 16496 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -4,6 +4,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
         for (int i = 1; i <= T; i++) {
@@ -11,9 +12,10 @@ public class Main {
             String A = st.nextToken();
             String B = st.nextToken();
             int C = Integer.parseInt(A) + Integer.parseInt(B);
-            
-            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + C);
+            bw.write("Case #" + i + ": " + A + " + " + B + " = " + C + "\n");
         }
+        bw.flush();
+        bw.close();
         br.close();
     }
 }
