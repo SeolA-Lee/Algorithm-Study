9b512691d90d3f784577e0ce5ab78f46e72a227d
Seol A <dlcodus1201@naver.com>
2025-03-07 20:13:54 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                sb.append(" ");
+            }
+            for (int m = 1; m <= i; m++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
