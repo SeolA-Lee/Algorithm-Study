197f949c07db985360cce61ad65b37e691f3f256
Seol A <dlcodus1201@naver.com>
2025-03-08 23:10:29 +0900

    [Bronze V] Title: A+B - 5, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,16 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         while (true) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
             if (A == 0 && B == 0) {
-                return;
+                break;
             }
-            System.out.println(A + B);
+            sb.append(A + B).append("\n");
         }
+        System.out.println(sb);
+        br.close();
     }
 }
