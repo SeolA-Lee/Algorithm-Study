856b07d120fa2644ebcc285da8e60298e129d113
Seol A <dlcodus1201@naver.com>
2025-03-13 21:18:23 +0900

    [Bronze V] Title: 문자열, Time: 104 ms, Memory: 13992 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,20 +1,19 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        String str;
-        for (int i = 0; i < t; i++) {
-            str = br.readLine();
-            int l = str.length();
-            if (l == 1) {
-                System.out.println(str + str);
-            } else {
-                System.out.print(str.charAt(0));
-                System.out.println(str.charAt(l-1));
-            }
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            sb.append(s.charAt(0)).append(s.charAt(s.length() - 1)).append('\n');
         }
+        System.out.println(sb);
         br.close();
     }
 }
