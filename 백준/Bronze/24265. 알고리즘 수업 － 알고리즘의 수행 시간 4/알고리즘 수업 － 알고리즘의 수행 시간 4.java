5f91a6d6018ef76d162de979c7c4ad3d6f7c0ae2
Seol A <dlcodus1201@naver.com>
2025-04-04 12:12:32 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -4,16 +4,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long n = Long.parseLong(br.readLine());
-        System.out.println(totalSum(n));
+        System.out.println((n-1) * (n) / 2);
         System.out.println(2);
         br.close();
-    }
-
-    public static long totalSum(long n) {
-        long sum = 0;
-        for (long i = 1; i < n; i++) {
-            sum += i;
-        }
-        return sum;
     }
 }
