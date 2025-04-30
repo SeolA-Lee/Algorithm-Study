555b3a8b1969c52ba106027c76fefa7a54cb72a0
Seol A <dlcodus1201@naver.com>
2025-04-30 18:46:44 +0900

    [Silver II] Title: 좌표 압축, Time: 1876 ms, Memory: 264264 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr1 = new int[N];
+        int[] arr2 = new int[N];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        br.close();
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            arr1[i] = x;
+            arr2[i] = x;
+        }
+
+        Arrays.sort(arr2);
+
+        int value = 0;
+        map.put(arr2[0], value);
+
+        for (int i = 1; i < N; i++) {
+            if (arr2[i] != arr2[i - 1]) value++;
+
+            if (!map.containsKey(arr2[i])) {
+                map.put(arr2[i], value);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(map.get(arr1[i])).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
