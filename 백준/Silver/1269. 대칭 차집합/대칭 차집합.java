37bd6e36ae664079a1f9ff6da3636282825f5225
Seol A <dlcodus1201@naver.com>
2025-05-02 01:36:54 +0900

    [Silver IV] Title: 대칭 차집합, Time: 708 ms, Memory: 86948 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> setA = new HashSet<>();
+        HashSet<Integer> setB = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < A; i++) {
+            setA.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < B; i++) {
+            setB.add(Integer.parseInt(st.nextToken()));
+        }
+
+        br.close();
+        
+        int countA = setA.size();
+        for (int val : setA) {
+            if (setB.contains(val)) {
+                countA--;
+            }
+        }
+        
+        int countB = setB.size();
+        for (int val : setB) {
+            if (setA.contains(val)) {
+                countB--;
+            }
+        }
+
+        System.out.println(countA + countB);
+    }
+}
